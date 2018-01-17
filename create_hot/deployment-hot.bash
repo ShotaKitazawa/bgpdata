52303e96dbe5f4db021bc8578a520e97be8cf5be
ShotaKitazawa <skitazawa1121@gmail.com>
2018-01-18 08:03:34 +0900

    fix

--- create_hot/deployment-hot.bash
+++ create_hot/deployment-hot.bash
@@ -5,12 +5,12 @@ SUB_COMMAND=${1:-"create/delete"}
 
 if [ "$SUB_COMMAND" = "create" ]; then
   for i in $(ls $(dirname $0)/network); do
-    openstack stack create -t $(dirname $0)/hot-network.yaml --parameter "network-name=$i" --parameter-file "cidr=$i" $i
+    openstack stack create -t $(dirname $0)/hot-network.yaml --parameter "network-name=$i" --parameter-file "cidr=$(dirname $0)/network/$i" as-$i
   done
   openstack stack create -t $(dirname $0)/hot-instances.yaml instances
 elif [ "$SUB_COMMAND" = "delete" ]; then
   for i in $(ls $(dirname $0)/network); do
-    openstack stack delete $i
+    openstack stack delete $i -y
   done
   openstack stack delete instances
 fi
