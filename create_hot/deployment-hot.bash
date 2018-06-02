5715600c8fdc6ad099eafb4df57c41e55f9b7dbd
ShotaKitazawa <skitazawa1121@gmail.com>
2018-06-02 21:09:29 +0900

    modify script

--- create_hot/deployment-hot.bash
+++ create_hot/deployment-hot.bash
@@ -2,14 +2,17 @@
 # Author: ShotaKitazawa
 
 SUB_COMMAND=${1:-"create/delete"}
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 if [ "$SUB_COMMAND" = "create" ]; then
-  for i in $(ls $(dirname $0)/network); do
-    openstack stack create -t $(dirname $0)/hot-network.yaml --parameter "network-name=$i" --parameter-file "cidr=$(dirname $0)/network/$i" as-$i
+  cd $SCRIPT_DIR/network
+  for i in *; do
+    openstack stack create -t $SCRIPT_DIR/hot-network.yaml --parameter "network-name=$i" --parameter-file "cidr=$SCRIPT_DIR/network/$i" as-$i
   done
   openstack stack create -t $(dirname $0)/hot-instances.yaml instances
 elif [ "$SUB_COMMAND" = "delete" ]; then
-  for i in $(ls $(dirname $0)/network); do
+  cd $SCRIPT_DIR/network
+  for i in *; do
     openstack stack delete as-$i -y
   done
   openstack stack delete instances
