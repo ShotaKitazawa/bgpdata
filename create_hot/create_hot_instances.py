24da9b3f611564661f1510a21a12981c801ea314
ShotaKitazawa <skitazawa1121@gmail.com>
2018-06-04 15:25:07 +0900

    fix hot: create vm after created volume

--- create_hot/create_hot_instances.py
+++ create_hot/create_hot_instances.py
@@ -28,33 +28,29 @@ for line in f:
     elif s == 1:
         s = 2
     elif s == 2:
-        # if AS == "15169":
-        #    s = 0
-        #    continue
+
+        g.write("  as{}_cinder_volume:\n".format(AS))
+        g.write("    type: OS::Cinder::Volume\n")
+        g.write("    properties:\n")
+        g.write("      name: as{}\n".format(AS))
+        g.write("      image: Ubuntu-Router\n")
+        g.write("      size: 10\n")
+        g.write("      availability_zone: nova\n")
+
         g.write("  as{0}:\n".format(AS))
         g.write("    type: OS::Nova::Server\n")
         g.write("    properties:\n")
         g.write("      name: as{}\n".format(str(AS)))
-        g.write("      image: Ubuntu-Router\n")
+        g.write("      key_name: default\n")
         g.write("      flavor: router\n")
+        g.write("      block_device_mapping_v2:\n")
+        g.write("        - volume_id: {{ get_resource: as{}_cinder_volume }}\n".format(AS))
         g.write("      networks:\n")
         for file in files:
             if "-" + AS in file or AS + "-" in file:
                 g.write("        - network: {0}\n".format(str(file)))
         g.write("      security_groups:\n")
         g.write("        - AllAllow\n")
-        g.write("      key_name: default\n")
-        g.write("  as{}_cinder_volume:\n".format(AS))
-        g.write("    type: OS::Cinder::Volume\n")
-        g.write("    properties:\n")
-        g.write("      size: 20\n")
-        g.write("      availability_zone: nova\n")
-        g.write("  as{}_volume_attachment:\n".format(AS))
-        g.write("    type: OS::Cinder::VolumeAttachment\n")
-        g.write("    properties:\n")
-        g.write("      volume_id: {{ get_resource: as{}_cinder_volume }}\n".format(AS))
-        g.write("      instance_uuid: {{ get_resource: as{} }}\n".format(AS))
-        g.write("      mountpoint: /dev/vda\n")
         g.write("\n")
 
         s = 0
