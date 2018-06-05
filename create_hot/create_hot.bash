5491af013db1cc9e4faaa1a576a37d65a7f0d467
ShotaKitazawa <skitazawa1121@gmail.com>
2018-06-05 10:55:02 +0900

    HOT on network integrated

--- create_hot/create_hot.bash
+++ create_hot/create_hot.bash
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 # Author: ShotaKitazawa
 
-ANALYZED_FILE=${1:-"err: invalid command: bash $0 analyzed_file"}
-echo "create HOT on network"
+ANALYZED_FILE=${1:?"err: invalid command: bash $0 analyzed_file"}
+echo "init all"
 rm -f $(dirname $0)/network/*
+echo "create values for HOT on network"
 python $(dirname $0)/create_values_network.py $ANALYZED_FILE
+echo "create HOT on network"
 python $(dirname $0)/create_hot_networks.py
 echo "create HOT on instances"
 rm -f $(dirname $0)/hot-instances.yaml
