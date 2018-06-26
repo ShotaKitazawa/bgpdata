ddcc67608342870f30226667a27c2ae860fa192a
ShotaKitazawa <skitazawa1121@gmail.com>
2018-06-26 19:11:19 +0900

    division hot for instances

--- deploy_hot/division_hot_instances.sh
+++ deploy_hot/division_hot_instances.sh
@@ -2,9 +2,13 @@
 
 TARGET_DIR=$(dirname $0)/instances
 MATCH="  script_asn[0-9]*_1:"
+FLAG=0
 
 if [ ! -d $TARGET_DIR ]; then mkdir -p $TARGET_DIR; fi
 rm -f $TARGET_DIR/*
+
+cp $(dirname $0)/netconf.sh $TARGET_DIR
+cp $(dirname $0)/gobgp.sh $TARGET_DIR
 
 cnt=0
 IFS=$'\n'; for line in $(cat $(dirname $0)/hot-instances.yaml); do
@@ -17,6 +21,9 @@ IFS=$'\n'; for line in $(cat $(dirname $0)/hot-instances.yaml); do
     echo "heat_template_version: pike" >> $FILENAME
     echo "resources:" >> $FILENAME
     FLAG=0
+  fi
+  if [ "$line" = "        - { get_resource: security_group_allallow }" ]; then
+    line="        - AllAllow"
   fi
   echo $line >> $FILENAME
 done
