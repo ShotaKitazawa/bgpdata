2736435c9e82759c57908715530c4f46fffa224f
ShotaKitazawa <skitazawa1121@gmail.com>
2018-07-07 23:11:49 +0900

    loにアドレスを付与する

--- deploy_hot/netconf.sh
+++ deploy_hot/netconf.sh
@@ -12,6 +12,15 @@ yes no | mv -i /etc/network/interfaces{,.default}
 echo "auto lo" >> /etc/network/interfaces
 echo "iface lo inet loopback" >> /etc/network/interfaces
 echo >> /etc/network/interfaces
+for i in $(seq 1 $(echo $ADVERTISE_ADDRS | wc -w)); do
+  NUM=$((i-1))
+  ADVERTISE_ADDR=$(echo $ADVERTISE_ADDRS | cut -d " " -f $i)
+  echo "auto lo:$NUM" >> /etc/network/interfaces
+  echo "iface lo:$NUM inet static" >> /etc/network/interfaces
+  echo "address $ADVERTISE_ADDR" >> /etc/network/interfaces
+
+  echo >> /etc/network/interfaces
+done
 for i in $(seq 1 $(echo $IFACES | wc -w)); do
   IFACE=$(echo $IFACES | cut -d " " -f $i)
   ADDR=$(echo $ADDRS | cut -d " " -f $i)
